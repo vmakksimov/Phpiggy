72b67aa56d114f01ae8d289e4b6a40d1363efd86
Viktor <vmakksimov@gmail.com>
2023-11-14 02:36:39 +0200

    adding Routes

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -2,10 +2,19 @@
 
 declare(strict_types= 1);
 
+
+
 require __DIR__ ."/../../vendor/autoload.php";
 
-use Framework\App;
+use Framework\{App, Router};
+
+
 
 $app = new App();
+
+
+$app -> get('/');
+
+dd($app);
 
 return $app;
---
+++ src/Framework/Router.php
@@ -0,0 +1,20 @@
+<?php
+
+
+declare(strict_types= 1);
+
+namespace Framework;
+
+class Router {
+
+    private array $routes = [];   
+    
+    
+    public function add(string $method, string $path){
+        $this->routes[] = [
+            'path'=> $path,
+            'method'=> strtoupper($method),
+        ];
+    }
+
+}
