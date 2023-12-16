e215f20d7f8971cffee481c0104eae99c69b4952
Viktor <vmakksimov@gmail.com>
2023-12-16 22:53:21 +0200

    register form

--- src/App/views/register.php
+++ src/App/views/register.php
@@ -2,7 +2,7 @@
 
 <section
   class="max-w-2xl mx-auto mt-12 p-4 bg-white shadow-md border border-gray-200 rounded"
->
+><?php var_dump($errors); ?>
   <form method="POST" class="grid grid-cols-1 gap-6"> 
     <!-- Email -->
     <label class="block">
@@ -12,6 +12,12 @@
         class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
         placeholder="john@example.com"
       />
+      
+      <?php if (array_key_exists('email', $errors)) : ?>
+        <div>
+          <?php echo e($errors['email'][0]); ?>
+        </div>
+      <?php  endif; ?>
     </label>
     <!-- Age -->
     <label class="block">
