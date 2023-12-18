df9b248764f81d361a54c0507ed3e40ca886d490
vmaksimov <viktor.maksimov@oblak.bg>
2023-12-18 11:30:59 +0200

    adding script

--- src/App/views/register.php
+++ src/App/views/register.php
@@ -2,35 +2,28 @@
 
 <section
   class="max-w-2xl mx-auto mt-12 p-4 bg-white shadow-md border border-gray-200 rounded"
->
+> <?php var_dump($errors); ?>
   <form method="POST" class="grid grid-cols-1 gap-6"> 
     <!-- Email -->
     <label class="block">
       <span class="text-gray-700">Email address</span>
-      <input value="<?php echo e($oldFormData['email'] ?? '');  ?>" name="email"
+      
+      <input name="email"
         type="email"
         class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
         placeholder="john@example.com"
       />
       <?php if (array_key_exists('email', $errors)) : ?>
-        <div style="color: red">
-          <?php echo e($errors['email'][0]); ?>
-        </div>
-      <?php  endif; ?>
+      <?php endif; ?>
     </label>
     <!-- Age -->
     <label class="block">
       <span class="text-gray-700">Age</span>
-      <input value="<?php echo e($oldFormData['age'] ?? '');  ?>" name="age"
+      <input name="age"
         type="number"
         class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
         placeholder=""
       />
-      <?php if (array_key_exists('age', $errors)) : ?>
-        <div style="color: red">
-          <?php echo e($errors['age'][0]); ?>
-        </div>
-      <?php  endif; ?>
     </label>
     <!-- Country -->
     <label class="block">
@@ -43,25 +36,15 @@
         <option value="Mexico">Mexico</option>
         <option value="Invalid">Invalid Country</option>
       </select>
-      <?php if (array_key_exists('country', $errors)) : ?>
-        <div style="color: red">
-          <?php echo e($errors['country'][0]); ?>
-        </div>
-      <?php  endif; ?>
     </label>
     <!-- Social Media URL -->
     <label class="block">
       <span class="text-gray-700">Social Media URL</span>
-      <input value="<?php echo e($oldFormData['socialMediaURL'] ?? '');  ?>" name="socialMediaURL"
+      <input name="socialMediaURL"
         type="text"
         class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
         placeholder=""
       />
-      <?php if (array_key_exists('socialMediaURL', $errors)) : ?>
-        <div style="color: red">
-          <?php echo e($errors['socialMediaURL'][0]); ?>
-        </div>
-      <?php  endif; ?>
     </label>
     <!-- Password -->
     <label class="block">
@@ -71,16 +54,6 @@
         class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
         placeholder=""
       />
-      <?php if (array_key_exists('password', $errors)) : ?>
-        <div style="color: red">
-          <?php echo e($errors['password'][0]); ?>
-        </div>
-      <?php  endif; ?>
-      <?php if (array_key_exists('confirmPassword', $errors)) : ?>
-        <div style="color: red">
-          <?php echo e($errors['confirmPassword'][0]); ?>
-        </div>
-      <?php  endif; ?>
     </label>
     <!-- Confirm Password -->
     <label class="block">
@@ -90,22 +63,16 @@
         class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
         placeholder=""
       />
-      <?php if (array_key_exists('confirmPassword', $errors)) : ?>
-        <div style="color: red">
-          <?php echo e($errors['confirmPassword'][0]); ?>
-        </div>
-      <?php  endif; ?>
     </label>
     <!-- Terms of Service -->
     <div class="block">
       <div class="mt-2">
         <div>
           <label class="inline-flex items-center">
-            <input <?php echo $oldFormData['tos'] ?? false ? 'checked' : '';  ?> name="tos"
+            <input name="tos"
               class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-offset-0 focus:ring-indigo-200 focus:ring-opacity-50"
               type="checkbox"
             />
-           
             <span class="ml-2">I accept the terms of service.</span>
           </label>
         </div>
